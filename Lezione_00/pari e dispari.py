a3ba32cbbb4b8908c97402e0193817a4f918c33d
mattyfil <luigi.cona@hotmail.it>
2025-02-21 11:27:00 +0100

    Add files via upload

---
+++ Lezione_00/pari e dispari.py
@@ -0,0 +1,29 @@
+pari=0
+dispari=0
+cont=0
+for cont in range(10):
+    n:float=float(input("scrivi valore"))
+    if n%2==0:
+        pari+=1
+        cont+=1
+        print(pari,dispari)
+    else:
+        dispari+=1
+        cont+=1
+        print(pari,dispari)
+      
+
+
+
+
+
+           
+
+
+
+        
+     
+     
+
+        
+
