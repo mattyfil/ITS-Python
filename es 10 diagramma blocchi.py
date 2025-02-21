d6e88e86ddd7bb4fe086156ca8d5d97341814e3c
mattyfil <luigi.cona@hotmail.it>
2025-02-21 11:28:01 +0100

    Add files via upload

---
+++ es 10 diagramma blocchi.py
@@ -0,0 +1,6 @@
+r:float=float(input("inserisci reddito"))
+m:float=float(input("inserisci media"))
+if r<20000 and m>27:
+    print("hai accesso alla borsa di studio")
+else:
+    print("borsa di studio rifiutata per reddito o media")
