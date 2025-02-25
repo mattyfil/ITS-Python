351cc498d916e9ac983ffe8ebbcc54625a923032
mattyfil <luigi.cona@hotmail.it>
2025-02-25 13:48:27 +0100

    Update animale:str=(input("inserisci l'animale".py

--- animale:str=(input("inserisci l'animale".py
+++ animale:str=(input("inserisci l'animale".py
@@ -1,4 +1,4 @@
-animale:str=(input("inserisci l'animale   "))
+eanimale:str=(input("inserisci l'animale   "))
 
 mammiferi:list=["cane","gatto","cavallo","elefante","leone","balena","delfino"]
 rettili:list=["serpente","lucertola","tartaruga","coccodrillo"]
@@ -10,22 +10,22 @@ habitat:list=["aria","acqua","terra"]
 
 match animale:
     case animale if animale in mammiferi and animale in (mammiferi[0:4]):
-        print(f"{animale} vive nella {habitat[2]}")
+        print(f"{animale} vive nella {habitat[2]} fa parte della categoria {mammiferi}")
     case animale if animale in mammiferi and animale in (mammiferi[5:7]):
-        print(f"{animale} vive nel {habitat[1]}")
+        print(f"{animale} vive nel {habitat[1]}" fa parte della categoria {mammiferi})
     case animale if animale in rettili and animale in (rettili[0:2]):
-        print(f"{animale} vive sulla{habitat[2]} ferma")
+        print(f"{animale} vive sulla{habitat[2]} ferma fa parte della categoria {rettilii}")
     case animale if animale in rettili and animale in (rettili[2:4]):
-        print(f"{animale} vive sull  {habitat[1]} e sulla {habitat[2]} ferma")
+        print(f"{animale} vive sull  {habitat[1]} e sulla {habitat[2]} ferma fa parte della categoria {rettili}")
     case animale if animale in uccelli and animale in (uccelli[0:3]):
-        print(f"{animale} vive nel {habitat[0]}")
+        print(f"{animale} vive nel {habitat[0]} fa parte della categoria {uccelli}")
     case animale if animale in uccelli and animale in (uccelli[4:6]):
-        print(f"{animale} vive nel {habitat[1]}")
+        print(f"{animale} vive nel {habitat[1]} fa parte della categoria {uccelli}")
     case animale if animale in uccelli and animale in uccelli[7]:
-        print(f"{animale} vive nel {habitat[2]}")
+        print(f"{animale} vive nel {habitat[2]} e fa parte della categoria {uccelli}")
     case animale if animale in pesci:
-        print(f"{animale} vive in {habitat[1]}")
-    
+        print(f"{animale} vive in {habitat[1]} fa parte della categoria {pesci}")
+            
     case _:
         print(f"fuma di meno")
 
